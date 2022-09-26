5c0be2f2a16fb64411630b08ed1153c9252ec128
Nguyen-Hai-254 <phamnguyenhai0123@gmail.com>
2022-09-26 16:09:22 +0700

    add todo list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './views/Nav';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
 
-  let [name, setName] = useState("Hai")
-  let [nameSubmit, setNameSubmit] = useState('')
+  let [name, setName] = useState("Hai");
+  let [nameSubmit, setNameSubmit] = useState('');
+  let [todo, setTodo] = useState([
+    { id: 'todo1', title: 'Playing game' },
+    { id: 'todo2', title: 'Doing homework' },
+    { id: 'todo3', title: 'Eating honey' }
+  ])
 
   const handleInputChange = (e) => {
     setNameSubmit(e.target.value);
@@ -14,6 +19,26 @@ function App() {
 
   const handleEventClick = () => {
     setName(nameSubmit);
+  }
+
+  const handleInputAddTodoChange = (e) => {
+    setNameSubmit(e.target.value);
+  }
+
+  const handleAddTodo = () => {
+    if (!nameSubmit)
+    {
+      alert('Empty Input');
+      return;
+    }
+    let n = todo.length;
+    let newTodo = {id: 'todo' + (n + 1), title: nameSubmit};
+
+    setTodo(
+      [...todo, newTodo]
+    );
+
+    setNameSubmit('')
   }
 
   return (
@@ -26,6 +51,18 @@ function App() {
         </p>
         <input type="text" onChange={(e) => handleInputChange(e)} />
         <button onClick={() => handleEventClick()}>Click me</button>
+
+        <div className='todoContainer'>
+          {todo &&
+            todo.map((todo, index) => {
+              return (
+                <li className="todoChild" key={todo.id}> {todo.title} </li>
+              )
+            })
+          }
+        </div>
+        <input type="text" value={nameSubmit} onChange={(e) => handleInputAddTodoChange(e)} />
+        <button onClick={() => handleAddTodo()}>Click me</button>
       </header>
     </div>
   );
