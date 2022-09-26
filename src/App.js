c0f7e8291024ba3e3580b69d04cbc934c4bd9e85
Nguyen-Hai-254 <phamnguyenhai0123@gmail.com>
2022-09-26 14:55:35 +0700

    state name, namesubmit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './views/Nav';
+import { useState } from 'react';
 
 function App() {
+
+  let [name, setName] = useState("Hai")
+  let [nameSubmit, setNameSubmit] = useState('')
+
+  const handleInputChange = (e) => {
+    setNameSubmit(e.target.value);
+  }
+
+  const handleEventClick = () => {
+    setName(nameSubmit);
+  }
+
   return (
     <div className="App">
       <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Waring!!!
+          Waring!!! Hello {name}
         </p>
+        <input type="text" onChange={(e) => handleInputChange(e)} />
+        <button onClick={() => handleEventClick()}>Click me</button>
       </header>
     </div>
   );
