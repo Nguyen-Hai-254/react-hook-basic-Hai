15f016d068a31db00ffd2508215f19f6ad06adf3
Nguyen-Hai-254 <phamnguyenhai0123@gmail.com>
2022-10-23 12:04:40 +0700

    add status loading, error, try and catch

--- src/views/Covid.js
+++ src/views/Covid.js
@@ -3,60 +3,72 @@ import axios from "axios";
 
 const Covid = () => {
     const [dataCovid, setDataCovid] = useState([]);
-
-    // useEffect(() => {
-    //     let res = axios.get('https://api.covid19api.com/country/vietnam?from=2021-10-01T00%3A00%3A00Z&to=2021-10-20T00%3A00%3A00Z')
-    //     .then(res => {
-    //         console.log(res.data);
-    //         let data = res && res.data ? res.data : []
-    //         console.log("data", data);
-    //         setDataCovid(data);
-    //         console.log("check", dataCovid);
-    //     })
-    //     // let data = res && res.data ? res.data : [];
-    //     // setDataCovid(data);
-    //     // console.log(dataCovid);})
-    //     // .then(setDataCovid(res.data))
-    // }, []);
+    const [loading, setLoading] = useState(true);
+    const [isError, setIsError] = useState(false);
 
     useEffect(() => {
         const loadData = async () => {
-            const res = await axios.get('https://api.covid19api.com/country/vietnam?from=2021-10-01T00%3A00%3A00Z&to=2021-10-20T00%3A00%3A00Z')
-            const data = res && res.data ? res.data : [];
-            setDataCovid(data);
+            try {
+                const res = await axios.get('https://api.covid19api.com/country/vietnam?from=2021-10-01T00%3A00%3A00Z&to=2021-10-20T00%3A00%3A00Z')
+                const data = res && res.data ? res.data : [];
+                setDataCovid(data);
+                setLoading(false);
+                setIsError(false);
+            }
+            catch (e) {
+                setIsError(true);
+                setLoading(false);
+                console.log('error: ', e.message);
+            }
         }
 
-        loadData();
+        setTimeout(() => {
+            loadData();
+        }, 2000);
+
     }, [])
 
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Confirmed</th>
-                    <th>Active</th>
-                    <th>Deaths</th>
-                    <th>Recovered</th>
-                </tr>
-            </thead>
-            <tbody>
-                {dataCovid && dataCovid.length > 0 &&
-                    dataCovid.map((item, index) => {
-                        const date = new Date(item.Date)
-                        return (
-                            <tr key={index}>
-                                <td>{date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear()}</td>
-                                <td>{item.Confirmed}</td>
-                                <td>{item.Active}</td>
-                                <td>{item.Deaths}</td>
-                                <td>{item.Recovered}</td>
-                            </tr>
-                        )
-                    })
-                }
-            </tbody>
-        </table>
+        <>
+            <h3>Covid 19 in VietNam</h3>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Confirmed</th>
+                        <th>Active</th>
+                        <th>Deaths</th>
+                        <th>Recovered</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {isError === false && loading === false && dataCovid && dataCovid.length > 0 &&
+                        dataCovid.map((item, index) => {
+                            const date = new Date(item.Date)
+                            return (
+                                <tr key={index}>
+                                    <td>{date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear()}</td>
+                                    <td>{item.Confirmed}</td>
+                                    <td>{item.Active}</td>
+                                    <td>{item.Deaths}</td>
+                                    <td>{item.Recovered}</td>
+                                </tr>
+                            )
+                        })
+                    }
+                    {loading &&
+                        <tr>
+                            <td>Loading data....</td>
+                        </tr>
+                    }
+                    {isError &&
+                        <tr>
+                            <td>Error....</td>
+                        </tr>
+                    }
+                </tbody>
+            </table>
+        </>
     )
 }
 
